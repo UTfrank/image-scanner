84319bd0e8c5ea6f54e7e472f1a8929bf4ab3052
Frank John <frankjoe261@gmail.com>
2025-11-19 10:34:51 +0100

    feat: implement file access

--- components/FIleUpload/index.tsx
+++ components/FIleUpload/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import * as DocumentPicker from 'expo-document-picker';
+import { File } from 'expo-file-system';
 
 const FileUpload = () => {
+  const handlePickDocument = async () => {
+    try{
+      const result = await DocumentPicker.getDocumentAsync({ copyToCacheDirectory: true });
+      const file = new File(result.assets[0]);
+      console.log(file.textSync());
+    } catch (err) {
+      console.log("Error picking document: ", err);
+    }
+  }
   return (
-    <TouchableOpacity activeOpacity={0.5} style={styles.button}>
-      <Text>Upload File</Text>
+    <TouchableOpacity activeOpacity={0.5} style={styles.button} onPress={handlePickDocument}>
+      <Text>Select File</Text>
     </TouchableOpacity>
   );
 };
